f4d67a6b5d6f91c090206df9334fbf05fa7ffd40
Kevin Aranda <kevinaranda042000@gmail.com>
2023-11-15 20:12:25 -0300

    se realizo el metodo index de categoría y además se creo el manejo de error en la carpeta Responses

--- Api-Compras/app/Http/Controllers/CategoryController.php
+++ Api-Compras/app/Http/Controllers/CategoryController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Responses\ApiResponse;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Exception;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {   //Retorna las Categorias existentes.
+
+        try{
+            $categories =  Category::all();
+            return ApiResponse::success('Lista de Categorias', 200, $categories);
+
+        } catch(Exception $e){
+            return ApiResponse::error('Error al obtener la lista de categorias: '.$e->getMessage(), 500);
+        }
+
+       
+    }
 }
