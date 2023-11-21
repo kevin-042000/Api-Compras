5fc5b7ac1287a46161ad21bf1765bebbfde0f9bb
Kevin Aranda <kevinaranda042000@gmail.com>
2023-11-21 11:47:02 -0300

    creación de las request de categoria, y de los metodos para el controlador categoria
    nota: falta el método destroy

--- Api-Compras/app/Http/Controllers/CategoryController.php
+++ Api-Compras/app/Http/Controllers/CategoryController.php
@@ -6,6 +6,10 @@ use App\Http\Responses\ApiResponse;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use Exception;
+use Illuminate\Validation\ValidationException;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class CategoryController extends Controller
 {
@@ -19,7 +23,45 @@ class CategoryController extends Controller
         } catch(Exception $e){
             return ApiResponse::error('Error al obtener la lista de categorias: '.$e->getMessage(), 500);
         }
-
        
     }
+
+    public function store(StoreCategoryRequest $request)
+    {
+       try{
+        $categoria = Category::create($request->all());
+        return ApiResponse::success('Categoria creada exitosamente', 201, $categoria);
+
+       } catch(ValidationException $e){
+        return ApiResponse::error('Error de validacion: '.$e->getMessage(), 422);
+       }
+    }
+
+    public function show($id)
+    {
+        try{
+            $categoria = Category::findOrFail($id);
+            return ApiResponse::success('Categoria obtinida exitosamente', 200, $categoria);
+
+        } catch(ModelNotFoundException $e){
+            return ApiResponse::error('Categoria no encontrada', 404);
+        }
+
+    }
+
+    public function update(UpdateCategoryRequest $request, $id)
+    {
+        try {
+            $categoria = Category::findOrFail($id); 
+            $categoria->update($request->all());
+            return ApiResponse::success('Categoria actualizada exitosamente', 200, $categoria);
+    
+        } catch (ModelNotFoundException $e) {
+            return ApiResponse::error('Categoria no encontrada', 404);
+        } catch (Exception $e){
+            return ApiResponse::error('Error al obtener la lista de categorias: '.$e->getMessage(), 500);
+
+        }
+    }
+
 }
